783f98876b221219e5a6dc540226b3ac9ad9be5e
Franklin <yusuffranklin@gmail.com>
2022-08-23 10:46:45 +0700

    Second Commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World! V1')
+    res.send('Hello World! V2')
 })
 
 app.listen(3000, () => {
